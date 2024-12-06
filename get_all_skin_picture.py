674828b8c441e7582077de236c6027f1697045fe
besane <1581096040@qq.com>
2024-12-06 19:01:00 +0800

    Add download all skins

--- get_all_skin_picture.py
+++ get_all_skin_picture.py
@@ -1,11 +1,8 @@
 # -*- codeing = utf-8 -*-
-from bs4 import BeautifulSoup  # 网页解析，获取数据
-import re  # 正则表达式，进行文字匹配`
-import urllib.request, urllib.error  # 制定URL，获取网页数据
-import xlwt  # 进行excel操作
 import requests
-from main import askURL
-import json
+import json, os
+from PIL import Image
+from io import BytesIO
 
 
 def translate_html_to_dict(url):
@@ -21,22 +18,46 @@ def translate_html_to_dict(url):
 
 
 def get_skin_addresses(datas):
-    addresses = []
+    download_fail_skins = []
     heroes = datas['hero'] # dict_keys(['hero', 'version', 'fileName', 'fileTime'])
     with open('skin_infos.txt', 'w+', encoding='utf-8') as file:
         i = 0
-        for hero in heroes:
+        for hero in heroes: # dict_keys(['heroId', 'name', 'alias', 'title', 'roles', 'isWeekFree', 'attack', 'defense', 'magic', 'difficulty', 'selectAudio', 'banAudio', 'isARAMweekfree', 'ispermanentweekfree', 'changeLabel', 'goldPrice', 'couponPrice', 'camp', 'campId', 'keywords', 'instance_id'])
             i = i + 1
             print(str(i) + '/' + str(len(heroes)))
-            # dict_keys(['heroId', 'name', 'alias', 'title', 'roles', 'isWeekFree', 'attack', 'defense', 'magic', 'difficulty', 'selectAudio', 'banAudio', 'isARAMweekfree', 'ispermanentweekfree', 'changeLabel', 'goldPrice', 'couponPrice', 'camp', 'campId', 'keywords', 'instance_id'])
-            file.write(hero['name'] + ' ' + hero['title'] + '\n')
-            hero_js_addr = 'https://game.gtimg.cn/images/lol/act/img/js/hero/' + hero['heroId'] + '.js?ts=2889127'
-            hero_detail = translate_html_to_dict(hero_js_addr)
+
+            # file.write(hero['name'] + ' ' + hero['title'] + '\n')
+            hero_js_addr = 'https://game.gtimg.cn/images/lol/act/img/js/hero/' + hero['heroId'] + '.js?ts=2889127' # 具体到某个英雄的js页面
+            hero_detail = translate_html_to_dict(hero_js_addr) # 获取英雄详细信息
+
             for skin in hero_detail['skins']:
-                if skin['chromas'] == '1':
+                if skin['chromas'] == '1': #炫彩
                     continue
-                print(skin['name'] + ' ' + skin['mainImg'] + '\n')
-                file.write(skin['name'] + ' ' + skin['mainImg'] + '\n')
+
+                # file.write(skin['name'] + ' ' + skin['mainImg'] + '\n')
+                filename = "D:\\LOL\\" + hero['name'] + hero['title'] + "\\" + skin['name'] + ".jpg"
+                filepath = "D:\\LOL\\" + hero['name'] + hero['title'] + "\\"
+                if not os.path.exists(filepath):
+                    os.makedirs(filepath)
+
+                address = skin['mainImg']
+                res = requests.get(address)
+                if res.status_code != 200: # 网页链接失败
+                    download_fail_skins.append(skin['name'])
+                    continue
+
+                # 保存图片
+                try:
+                    image = Image.open(BytesIO(res.content))
+                    image.save(filename)
+                    print('成功下载' + skin['name'] + '.jpg')
+                except:
+                    filename = filename.replace('/', '')
+                    image = Image.open(BytesIO(res.content))
+                    rgb_image = image.convert('RGB')
+                    rgb_image.save(filename)
+                    print('成功下载' + skin['name'] + '.jpg')
+    print(f'Failed download pictures[{len(download_fail_skins)}:', download_fail_skins)
 
 
 if __name__ == '__main__':
